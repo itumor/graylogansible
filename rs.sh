298529ce6e2e17aa991938d4fb5ae4907422a2fa
root <root@huma20050002hti28-gmail-com6.mylabserver.com>
2016-12-29 17:43:22 +0000

    rep working with sync

--- rs.sh
+++ rs.sh
@@ -6,9 +6,7 @@ mongo --eval 'rs.initiate(
       _id: "rs01",
       version: 1,
       members: [
-         { _id: 0, host : "172.31.44.165:27017" },
-         { _id: 1, host : "172.31.45.7:27017" },
-         { _id: 2, host : "172.31.45.2:27017" }
+         { _id: 0, host : "172.31.44.165:27017" }
       ]
    }
 )'
@@ -18,9 +16,7 @@ mongo --eval 'rs.reconfig(
       _id: "rs01",
       version: 1,
       members: [
-         { _id: 0, host : "172.31.44.165:27017" },
-         { _id: 1, host : "172.31.45.7:27017" },
-         { _id: 2, host : "172.31.45.2:27017" }
+         { _id: 0, host : "172.31.44.165:27017" }
       ]
    }
-)'	
+,{ force: true })'	
